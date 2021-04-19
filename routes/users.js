a4e90c07fe068a0eda16b17ad04b698e4cf7e16d
Thuanny <thucatm@gmail.com>
2021-04-19 19:01:59 -0300

    Persistência de dados com NeDB, bando de dados javascript

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,9 @@
+let NeDB = require('nedb');
+let db = new NeDB({
+    filename:'users.db',
+    autoload:true
+})
+
 module.exports = (app)=>{
 
     app.get('/users', (req, res) => {
@@ -14,10 +20,19 @@ module.exports = (app)=>{
     
     });
     
-    app.post('/users', (req, res)=> {
-    
-        
-        res.json(req.body);
+    app.post('/users', (req, res)=> {      
+
+        db.insert(req.body, (err, user)=>{
+
+            if(err) {
+                console.log(`error: ${err}`);
+                res.status(400).json({
+                    error: err
+                });
+            } else {
+                res.status(200).json(user);
+            }
+        })
     
     
     });
